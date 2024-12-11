6226302faf14921b133234c98c1f65ab4fad30d7
philip <pbergman@live.nl>
2024-12-11 21:30:12 +0100

    init

---
+++ README.md
@@ -0,0 +1,57 @@
+Mijn Host module for Caddy
+===========================
+
+This package contains a DNS provider module for [Caddy](https://github.com/caddyserver/caddy). It can be used to manage DNS records with [mijn host](https://mijn.host/).
+It makes use of [pbergman/libdns-mijn-host](https://github.com/pbergman/libdns-mijn-host)
+
+## Caddy module name
+
+```
+dns.providers.mijn-host
+```
+
+## Config examples
+
+To use this module for the ACME DNS challenge, [configure the ACME issuer in your Caddy JSON](https://caddyserver.com/docs/json/apps/tls/automation/policies/issuer/acme/) like so:
+
+```json
+{
+	"module": "acme",
+	"challenges": {
+		"dns": {
+			"provider": {
+				"name": "mijn-host",
+				"api_key": "YOUR_API_KEY"
+			}
+		}
+	}
+}
+```
+
+or with the Caddyfile:
+
+```
+# globally
+{
+	acme_dns mijn-host <api_key>
+}
+```
+
+```
+# one site
+tls {
+	dns mijn-host <api_key>
+}
+```
+
+or alternatively:
+
+
+```
+tls {
+	dns transip {
+		account_name <accountName> 
+		private_key_path <privateKeyPath>
+	}
+}
+```
---
+++ module.go
@@ -0,0 +1,73 @@
+package template
+
+import (
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	mijn_host "github.com/pbergman/libdns-mijn-host"
+)
+
+// Provider lets Caddy read and manipulate DNS records hosted by this DNS provider.
+type Provider struct{ *mijn_host.Provider }
+
+func init() {
+	caddy.RegisterModule(Provider{})
+}
+
+// CaddyModule returns the Caddy module information.
+func (Provider) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "dns.providers.mijn-host",
+		New: func() caddy.Module { return &Provider{mijn_host.NewProvider("")} },
+	}
+}
+
+// Provision sets up the module. Implements caddy.Provisioner.
+// TODO: This is just an example. Useful to allow env variable placeholders; update accordingly.
+func (p *Provider) Provision(ctx caddy.Context) error {
+	//repl := caddy.NewReplacer()
+	return nil
+}
+
+// UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
+//
+//	providername [<private_key_path>] {
+//	    api_token <api_token>
+//	}
+func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+
+	caddy.Log()
+	for d.Next() {
+		if d.NextArg() {
+			p.Provider.SetApiKey(d.Val())
+		}
+		if d.NextArg() {
+			return d.ArgErr()
+		}
+		for nesting := d.Nesting(); d.NextBlock(nesting); {
+			switch d.Val() {
+			case "api_key":
+				if p.Provider.GetApiKey() != "" {
+					return d.Err("ApiKey already set")
+				}
+				if d.NextArg() {
+					p.Provider.SetApiKey(d.Val())
+				}
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			default:
+				return d.Errf("unrecognized subdirective '%s'", d.Val())
+			}
+		}
+	}
+	if p.Provider.GetApiKey() == "" {
+		return d.Err("Missing ApiKey")
+	}
+	return nil
+}
+
+// Interface guards
+var (
+	_ caddyfile.Unmarshaler = (*Provider)(nil)
+	_ caddy.Provisioner     = (*Provider)(nil)
+)
