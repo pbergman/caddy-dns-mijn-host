47d35d6ae2d8b02f15148efa6e0ddce091bbaec2
philip <pbergman@live.nl>
2024-12-16 14:37:01 +0100

    update lib

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.22.9
 
 require (
 	github.com/caddyserver/caddy/v2 v2.8.4
-	github.com/pbergman/libdns-mijn-host v0.0.0-20241213135138-3a5aca585c70
+	github.com/pbergman/libdns-mijn-host v0.0.0-20241216132242-bf7c71367d06
 )
 
 require (
--- module.go
+++ module.go
@@ -1,6 +1,8 @@
 package template
 
 import (
+	"os"
+
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	mijn_host "github.com/pbergman/libdns-mijn-host"
@@ -10,7 +12,7 @@ import (
 type Provider struct{ *mijn_host.Provider }
 
 func init() {
-	caddy.RegisterModule(Provider{})
+	caddy.RegisterModule((*Provider)(nil))
 }
 
 // CaddyModule returns the Caddy module information.
@@ -23,19 +25,20 @@ func (Provider) CaddyModule() caddy.ModuleInfo {
 
 // Provision sets up the module. Implements caddy.Provisioner.
 func (p *Provider) Provision(ctx caddy.Context) error {
-	p.Provider.ApiKey = caddy.NewReplacer().ReplaceAll(p.Provider.ApiKey, "")
+	p.Provider.SetApiKey(caddy.NewReplacer().ReplaceAll(p.Provider.GetApiKey(), ""))
 	return nil
 }
 
 // UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
 //
-//	providername  [<api_key> <private_key_path>] {
-//	    api_key <api_key>
-//	}
+//		providername  [<api_key>] {
+//		    api_key <api_key>
+//	     debug   <true|false>
+//		}
 func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	for d.Next() {
 		if d.NextArg() {
-			p.Provider.ApiKey = d.Val()
+			p.Provider.SetApiKey(d.Val())
 		}
 		if d.NextArg() {
 			return d.ArgErr()
@@ -43,11 +46,25 @@ func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 		for nesting := d.Nesting(); d.NextBlock(nesting); {
 			switch d.Val() {
 			case "api_key":
-				if p.Provider.ApiKey != "" {
+				if p.Provider.GetApiKey() != "" {
 					return d.Err("ApiKey already set")
 				}
 				if d.NextArg() {
-					p.Provider.ApiKey = d.Val()
+					p.Provider.SetApiKey(d.Val())
+				}
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			case "debug":
+				if d.NextArg() {
+					value := d.ScalarVal()
+					if x, o := value.(bool); o {
+						if x {
+							p.Provider.SetDebug(os.Stdout)
+						} else {
+							p.Provider.SetDebug(nil)
+						}
+					}
 				}
 				if d.NextArg() {
 					return d.ArgErr()
@@ -57,7 +74,7 @@ func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 			}
 		}
 	}
-	if p.Provider.ApiKey == "" {
+	if p.Provider.GetApiKey() == "" {
 		return d.Err("Missing ApiKey")
 	}
 	return nil
